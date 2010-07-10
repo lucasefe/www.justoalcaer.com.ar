d2be6d5bab5a9624d3f822a0dd24326fd7196a85
Lucas Florio <lucasefe@gmail.com>
2010-07-10 20:01:19 -0300

    Agregando fotos de Libario 2010

--- _site/js/app.js
+++ _site/js/app.js
@@ -11,6 +11,7 @@ var JustoAlCaer = {
     );
   },
   getFlickr: function() {
+    JustoAlCaer.getFlickrPhotosetInto('#libario-2010', '72157624341248883');
     JustoAlCaer.getFlickrPhotosetInto('#olivos-2010', '72157624287250066');
     JustoAlCaer.getFlickrPhotosetInto('#libario-2009', '72157624133710654');
     JustoAlCaer.getFlickrPhotosetInto('#kronopios-2009', '72157624141187002');
